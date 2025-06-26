2f426eca4e306da98bd05932bcf65b162428e89f
Silje32 <msh070425@gmail.com>
2025-06-26 09:47:02 +0200

    Added the variables

---
+++ app.js
@@ -0,0 +1,20 @@
+const soundMap = {
+  q: "work_it",
+  w: "make_it",
+  e: "do_it",
+  r: "makes_us",
+  t: "harder",
+  y: "better",
+  u: "faster",
+  i: "stronger",
+  o: "more_than",
+  p: "hour",
+  a: "our",
+  s: "never",
+  d: "ever",
+  f: "after",
+  g: "work_is",
+  h: "over",
+};
+
+const drumContainer = document.querySelector("#drum-container");
